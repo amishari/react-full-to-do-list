11529bdcd6c8d08e129034c9dc3eadbda1fa34a4
amishari <amishari@yahoo.com>
2022-12-18 22:27:41 +0330

    conditions added

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ export default function App() {
       title: e.target.value,
       status: updated.status ? true : false,
     };
+
     setUpdated(newEntry);
   };
 
@@ -51,64 +52,70 @@ export default function App() {
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <div className="addTask">
-        <input
-          className=""
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button className="" onClick={addHandler}>
-          Add
-        </button>
-      </div>
-
-      <div className="update">
-        <input
-          className=""
-          value={updated.title}
-          onChange={(e) => changeTask(e)}
-        />
-        <div className="actions">
-          <button onClick={editHandler}>Update</button>
-          <button onClick={cancelHandler}>Cancel</button>
+      {updated && updated ? (
+        <div className="update">
+          <input
+            className=""
+            value={updated.title}
+            onChange={(e) => changeTask(e)}
+          />
+          <div className="actions">
+            <button onClick={editHandler}>Update</button>
+            <button onClick={cancelHandler}>Cancel</button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="addTask">
+          <input
+            className=""
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button className="" onClick={addHandler}>
+            Add
+          </button>
+        </div>
+      )}
 
       {toDo ? "" : "Nothing to display!"}
-      {toDo.map((task, index) => {
-        return (
-          <div className="task-wrap">
-            <div className={!task.status ? "tasks" : "done"}>
-              <span className="task-number">{index + 1}</span>
-              <span className="task-title">{task.title}</span>
-            </div>
+      {toDo &&
+        toDo.map((task, index) => {
+          return (
+            <div key={task.id} className="task-wrap">
+              <div className={!task.status ? "tasks" : "done"}>
+                <span className="task-number">{index + 1}</span>
+                <span className="task-title">{task.title}</span>
+              </div>
 
-            <span className="iconwrap">
-              <FaCheckCircle
-                className="mark"
-                onClick={(id) => markHandler(task.id)}
-                title="Done"
-              />
-              <FaPen
-                className="edit"
-                onClick={() =>
-                  setUpdated({
-                    id: task.id,
-                    title: task.title,
-                    status: task.status ? true : false,
-                  })
-                }
-                title="Edit"
-              />
-              <FaTrashAlt
-                className="del"
-                onClick={(id) => delHandler(task.id)}
-                title="Delete"
-              />
-            </span>
-          </div>
-        );
-      })}
+              <span className="iconwrap">
+                <FaCheckCircle
+                  className="mark"
+                  onClick={(id) => markHandler(task.id)}
+                  title="Done"
+                />
+                {task.status ? null : (
+                  <FaPen
+                    className="edit"
+                    onClick={() =>
+                      setUpdated({
+                        id: task.id,
+                        title: task.title,
+                        status: task.status ? true : false,
+                      })
+                    }
+                    title="Edit"
+                  />
+                )}
+
+                <FaTrashAlt
+                  className="del"
+                  onClick={(id) => delHandler(task.id)}
+                  title="Delete"
+                />
+              </span>
+            </div>
+          );
+        })}
     </div>
   );
 }
