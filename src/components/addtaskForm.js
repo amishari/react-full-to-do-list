180d5140ac3acc321e2771b09d5462f3a2caaead
amishari <81028814+amishari@users.noreply.github.com>
2023-01-01 09:22:38 +0330

    Latex Conditions added

--- src/components/addtaskForm.js
+++ src/components/addtaskForm.js
@@ -1,19 +1,23 @@
 import "../styles.css";
-const AddTaskForm =({ newTask, setNewTask, addHandler})=>{
-    return(
-       
-       <div className="addTask">
-          <input
-            className=""
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-          />
-          <button className="" onClick={addHandler}>
-            Add
-          </button>
-        </div>
-        
-    )
-
-}
+const AddTaskForm = ({ newTask, setNewTask, addHandler }) => {
+  let lettets = /^[0-9-\u0600-\u06FF\s]+$/; /*only En Char: /^[a-zA-Z]+$/ */
+  return (
+    <div className="addTask">
+      <input
+        className={
+          newTask.length === 0
+            ? ""
+            : newTask.match(lettets)
+            ? "isCorrect"
+            : "isError"
+        }
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button className="addBtn" onClick={addHandler}>
+        Add
+      </button>
+    </div>
+  );
+};
 export default AddTaskForm;
